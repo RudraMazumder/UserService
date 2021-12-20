61f2a1de9f3d85aaa5c85089d5f0ad9cd2e855cc
Rudra Mazumder <rudramwk@gmail.com>
2021-12-20 12:44:45 +0100

    -

--- src/test/java/com/example/demo/DemoSpringSecurityApplicationTests.java
+++ src/test/java/com/example/demo/DemoSpringSecurityApplicationTests.java
@@ -37,43 +37,66 @@ class DemoSpringSecurityApplicationTests {
 	@Autowired
 	private MockMvc mockMvc;
 
-
-
 	@Test
 	void GetWithAuthorizedUser() throws Exception {
-		if(mockMvc==null)
-			log.info("mockMvc is null");
-		
+
 		MvcResult mvcResult = mockMvc.perform(formLogin().user("john").password("pwd"))
 				.andExpect(authenticated().withUsername("john")).andReturn();
 
 		MockHttpSession session = (MockHttpSession) mvcResult.getRequest().getSession(false);
 		mockMvc.perform(get("/api/users").session(session)).andExpect(authenticated()).andExpect(status().isOk());
 	}
+	
+	@Test
+	void GetWithUnAuthorizedUser() throws Exception {
+
+		MvcResult mvcResult = mockMvc.perform(formLogin().user("paul").password("pwd"))
+				.andExpect(authenticated().withUsername("paul")).andReturn();
+
+		MockHttpSession session = (MockHttpSession) mvcResult.getRequest().getSession(false);
+		mockMvc.perform(get("/api/users").session(session)).andExpect(authenticated()).andExpect(status().isForbidden());
+	}
 
 	@BeforeEach
 	public void populateUsers() {
-		User user = new User(null, "john", "John Doe", passwordEncoder.encode("pwd"),
-				Arrays.asList(new Role(null, "USER")));
-		log.info("user {} to be added with role {}", user.getUsername(), user.getRoles());
-		userRepo.save(user);
-		log.info("user {} added with role {}", user.getUsername(), user.getRoles());
+		User userEntity = userRepo.getUserByUsername("john");
 
+		if (userEntity == null) {
+
+			User user = new User(null, "john", "John Doe", passwordEncoder.encode("pwd"),
+					Arrays.asList(new Role(null, "USER")));
+
+			log.info("user {} to be added with role {}", user.getUsername(), user.getRoles());
+			userRepo.save(user);
+			log.info("user {} added with role {}", user.getUsername(), user.getRoles());
+
+		}
 		// user creation with Role ADMIN
-		User admin = new User(null, "dave", "David Root", passwordEncoder.encode("pwd"),
-				Arrays.asList(new Role(null, "ADMIN")));
-		log.info("user {} to be added with role {}", admin.getUsername(), admin.getRoles());
-		userRepo.save(admin);
-		log.info("user {} added with role {}", admin.getUsername(), admin.getRoles());
 
+		User daveEntity = userRepo.getUserByUsername("dave");
+
+		if (daveEntity == null) {
+
+			User admin = new User(null, "dave", "David Root", passwordEncoder.encode("pwd"),
+					Arrays.asList(new Role(null, "ADMIN")));
+			log.info("user {} to be added with role {}", admin.getUsername(), admin.getRoles());
+			userRepo.save(admin);
+			log.info("user {} added with role {}", admin.getUsername(), admin.getRoles());
+		}
 		// user creation with Role MANAGER
-		User manager = new User(null, "paul", "Paul Davies", passwordEncoder.encode("pwd"),
-				Arrays.asList(new Role(null, "MANAGER")));
-		log.info("user {} to be added with role {}", manager.getUsername(), manager.getRoles());
-		userRepo.save(manager);
-		log.info("user {} added with role {}", manager.getUsername(), manager.getRoles());
+
+		User paulEntity = userRepo.getUserByUsername("paul");
+
+		if (paulEntity == null) {
+			User manager = new User(null, "paul", "Paul Davies", passwordEncoder.encode("pwd"),
+					Arrays.asList(new Role(null, "MANAGER")));
+			log.info("user {} to be added with role {}", manager.getUsername(), manager.getRoles());
+			userRepo.save(manager);
+			log.info("user {} added with role {}", manager.getUsername(), manager.getRoles());
+		}
+
 	}
-	
+
 	/*
 	 * @Bean(name = "passwordEncoder") public PasswordEncoder encoder() { return new
 	 * BCryptPasswordEncoder(); }
